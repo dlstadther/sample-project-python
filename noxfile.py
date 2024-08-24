1bf44a0f49815b51f669085f6a01e2ea4dfd20a4
Dillon Stadther <dlstadther@gmail.com>
2024-08-23 21:59:34 -0400

    feat: replace poetry with uv (#5)

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,22 @@
 import nox
 
 
-def _install_poetry(session: nox.Session) -> nox.Session:
-    session.install("poetry")
-    session.run("poetry", "install")
+def _install_package_manager(session: nox.Session) -> nox.Session:
+    session.install("uv")
+    session.run("uv", "sync")
     return session
 
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    session = _install_poetry(session=session)
+    session = _install_package_manager(session=session)
     session.run("coverage", "run", "-m", "pytest", "-vv")
     session.run("coverage", "report")
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session = _install_poetry(session=session)
+    session = _install_package_manager(session=session)
     session.run("ruff", "format", "--check", ".")
     session.run("ruff", "check", "--output-format=github", ".")
     session.run("sqlfluff", "lint", ".")
@@ -24,5 +24,5 @@ def lint(session: nox.Session) -> None:
 
 @nox.session
 def typing(session: nox.Session) -> None:
-    session = _install_poetry(session=session)
+    session = _install_package_manager(session=session)
     session.run("mypy", ".")
